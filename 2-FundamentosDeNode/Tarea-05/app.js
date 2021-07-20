f7beb2cbc8629b63948c18f2169976dbd2b479fa
xcodebanda <xcode.banda@gmail.com>
2021-07-20 13:30:42 -0300

    Tarea 5

---
+++ 2-FundamentosDeNode/Tarea-05/app.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+// third party modules
+const colors = require('colors')
+
+// app modules
+const tablaMultiplicar = require('./modulos/tablaMultiplicar')
+const obtenerConsola = require('./modulos/obtenerConsola')
+const crearArchivo = require('./modulos/crearArchivo')
+
+const base = obtenerConsola('--base')
+const nombreArchivo = `tabla-${base}.txt`
+const direccion = path.join(__dirname, nombreArchivo)
+
+const tabla = tablaMultiplicar(base)
+const titulo = colors.bgCyan(`Tabla del ${base}\n`)
+console.log(titulo, tabla)
+
+crearArchivo(direccion, tabla)
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
